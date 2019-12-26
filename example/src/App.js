6ed8b76efa4332831236d2a72c404a3ac8c0a6fa
Thiago Valentim <thiago@criawebs.com.br>
2019-12-25 22:29:42 -0300

    chore: update the example of use

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'ag-cumulative-flow-lean-stats'
+import StatCard from 'ag-cumulative-flow-lean-stats'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <StatCard number='2' statType='leadTime' statName='Lead time' />
       </div>
     )
   }
