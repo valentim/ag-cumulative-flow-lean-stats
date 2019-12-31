f170f28d23899f1b729b7f1dbf45263d1b8c4f5f
Thiago Valentim <thiago@criawebs.com.br>
2019-12-30 22:54:18 -0300

    chore: implement the new type of card, Period and Quantity

--- example/src/App.js
+++ example/src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react'
 
-import StatCard from 'ag-cumulative-flow-lean-stats'
+import { QuantityCard, PeriodCard, leadTime, cycleTime } from 'ag-cumulative-flow-lean-stats'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <StatCard number='2' statType='leadTime' statName='Lead time' />
-        <StatCard number='2' statType='cycleTime' statName='Cycle time' />
-        <StatCard number='2' statName='Backlog' />
-        <StatCard number='2' statName='WIP' />
-
+        <PeriodCard number='12000' lang="pt" statType={leadTime} statName='Lead time' />
+        <PeriodCard number='6600' statType={cycleTime} statName='Cycle time' />
+        <QuantityCard number='2' statName='Backlog' />
+        <QuantityCard number='2' statName='WIP' />
       </div>
     )
   }
